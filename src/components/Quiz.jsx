ac4e5e9521affa6154e8f706e91434e5ba4f34d3
WelintonJunior <welintonjr011@gmail.com>
2024-03-07 08:31:48 -0300

    Added a Question Timer, but not completely functional

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react"
 import QUESTIONS from "../questions.js"
 import img from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([])
 
     const activeQuestionIndex = userAnswers.length;
-    
+
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
-    
+
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [
@@ -18,20 +19,21 @@ export default function Quiz() {
             ]
         })
     }
-    
-    if(quizIsComplete) {
+
+    if (quizIsComplete) {
         return <div id="summary">
             <img src={img} alt="Complete" />
             <h2>Quiz is Complete</h2>
         </div>
     }
-   
+
     const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
     shuffledAnswers.sort(() => Math.random() - 0.5)
 
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer timeout={15000} onTimeout={() => handleSelectAnswer(null)} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {
